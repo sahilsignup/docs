7a11402330023c1b38ec00b17e883c6395e9d530
Georgy Marchuk <gmarcuk@gmail.com>
2020-06-29 03:25:19 +0200

    wip

--- api/validation-endpoint.js
+++ api/validation-endpoint.js
@@ -1,23 +1,24 @@
+const validateCommand = require('@swup/cli/src/commands/validate');
 let output = '';
 
 console.log = (str) => {
     output += (`${str}\n`);
 };
 
+validateCommand.flags = {
+    ...validateCommand.flags,
+    findUrls: {
+        ...validateCommand.flags.findUrls,
+        default: 'http://swup.js.org/'
+    },
+    asynchronous: {
+        ...validateCommand.flags.asynchronous,
+        default: true,
+    }
+};
+
 exports.handler = async function(event, context, callback) {
     const params = new URLSearchParams(event.body);
-    const validateCommand = require('@swup/cli/src/commands/validate');
-    validateCommand.flags = {
-        ...validateCommand.flags,
-        findUrls: {
-            ...validateCommand.flags.findUrls,
-            default: 'http://swup.js.org/'
-        },
-        asynchronous: {
-            ...validateCommand.flags.asynchronous,
-            default: true,
-        }
-    };
     const mainUrl = params.get('main_url');
 
     await validateCommand.run();
